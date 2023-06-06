0f937df11a25d5708bec7fc50fae669096c25510
ayushmishra <mail81mitthu@gmail.com>
2023-06-06 16:46:21 +0530

    added manual event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os.path
-import datetime as dt 
+import sys
+import datetime as dt
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -8,6 +9,7 @@ from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
 SCOPES = ["https://www.googleapis.com/auth/calendar"]
+
 
 def main():
     creds = None
@@ -29,22 +31,55 @@ def main():
     try:
         service = build("calendar", "v3", credentials=creds)
 
-        now = dt.datetime.now().isoformat() + "Z"
+        if sys.argv[1] == "show":
+            now = dt.datetime.now().isoformat() + "Z"
 
-        # TODO: get the use to enter the maxReuslt
-        event_result = service.events().list(calendarId="primary", timeMin=now, maxResults=20, singleEvents=True, orderBy="startTime").execute()
-        events = event_result.get("Itrms", [])
+            # TODO: get the use to enter the maxReuslt
+            event_result = (
+                service.events()
+                .list(
+                    calendarId="primary",
+                    timeMin=now,
+                    maxResults=20,
+                    singleEvents=True,
+                    orderBy="startTime",
+                )
+                .execute()
+            )
+            events = event_result.get("Itrms", [])
 
-        if not events:
-            print("No up comming evetns")
-            return
-        for event in events:
-            start = event("start").get("dateTime", event["start"].get["date"])
+            if not events:
+                print("No up comming evetns")
+                return
+            for event in events:
+                start = event("start").get("dateTime", event["start"].get["date"])
 
-            print(start, event["summary"])
+                print(start, event["summary"])
+        elif sys.argv[1] == "add":
+            # TODO get user to add events
+            events = {
+                "summary": "It is working",
+                "location": "Home",
+                "description": "Deez nuts",
+                "colorId": 6,
+                "start": {
+                    #get user to enter the timeMin
+                    "dateTime": "2023-06-03f09:00:00+02:00",
+                    "timeZone": "Kolkata"
+                },
+                "end": {
+                    #get user to enter the timeMin
+                    "dateTime": "2023-06-03f12:00:00+02:00",
+                    "timeZone": "Kolkata"
+                },
+                "recurrence": [
+                    ""
+                    ]
+            }
 
     except HttpError as error:
         print("Error: ", error)
+
 
 if __name__ == "__main__":
     main()
